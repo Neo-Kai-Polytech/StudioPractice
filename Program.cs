77aab8f1b159cff05c48f702f66b74af92648855
Alina Nicolaisen (1000136919) <NICOA3@student.op.ac.nz>
2025-05-06 13:43:39 +1200

    added Alinas loop

--- Program.cs
+++ Program.cs
@@ -21,12 +21,20 @@ namespace Studio_1_test
             //Alina
             Console.WriteLine("Hi, my name is Alina!")
             Console.WriteLine("I'm 21 years old.");
+            int banana = 0;
+            for (int i = 0; i < 10; i++) {
+                banana++;  
+                Console.WriteLine("Iteration " + i + " : " banana + " bananas");
+            }
+
+            Console.WriteLine("Total: " + banana + " bananas");
+
 
 
 
             //Neo
             Console.WriteLine("This is a line of code that will print a things oh yes it is");
             Console.ReadLine();
-        }
+        
     }
 }
