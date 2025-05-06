4df05ad6f981571ae5e46ddd8abff34d24569a76
Catherine Gizzi (1000099545) <GIZZCM1@student.op.ac.nz>
2025-05-06 13:25:05 +1200

    Adding CS file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Studio_1_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
